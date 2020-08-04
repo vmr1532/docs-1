79a7814404ea8aba40902d66933541662c0613a9
narendra-pareek <narendra.pareek@loginradius.com>
2020-08-04 16:08:36 +0530

    phonegap document

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,8 @@ module.exports = {
           ],
           "Mobile SDK": [
             'mobile-sdk-libraries/android-library',
-            'mobile-sdk-libraries/ios-library'
+            'mobile-sdk-libraries/ios-library',
+            'mobile-sdk-libraries/phonegap-library'
           ],
           "How to": [
             "howto/dashboard-setup",
